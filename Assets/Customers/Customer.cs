d523434ce1066e04a83e461d62009ed58a86789c
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-05-14 13:30:10 -0400

    customer coding

--- Assets/Customers/Customer.cs
+++ Assets/Customers/Customer.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +6,13 @@ public class Customer : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Animator animator;
     private Vector3 target;
+
+    private Vector3 normalizedMovement;
+    private Vector3 forwardVector;
+    private Vector3 rightVector;
+
+    [SerializeField] Vector3[] possibleActions;
+    [SerializeField] string[] possibleActionNames;
 
     void Start()
     {
@@ -20,26 +26,44 @@ public class Customer : MonoBehaviour
     {
         SetTargetPosition();
 
-        if (target.y > transform.position.y) { animator.SetBool("Up", true); }
-        else { animator.SetBool("Up", false); }
-
-        if (target.y < transform.position.y) { animator.SetBool("Down", true); }
-        else { animator.SetBool("Down", false); }
-
-        if (target.x > transform.position.x) { animator.SetBool("Right", true); }
-        else { animator.SetBool("Right", false); }
-
-        if (target.x < transform.position.x) { animator.SetBool("Left", true); }
-        else { animator.SetBool("Left", false); }
+        Animate();
 
         SetAgentPosition();
     }
 
-    private void SetTargetPosition() { target = Camera.main.ScreenToWorldPoint(Input.mousePosition); }
+    private void SetTargetPosition()
+    {
+        int rnd = Random.Range(1, possibleActions.Length);
+        print(gameObject.name + " = " + GetActionName(rnd));
+
+        target = GetAction(rnd);
+    }
+
+    private Vector3 GetAction(int rndNum) { return possibleActions[rndNum - 1]; }
+    private string GetActionName(int rndNum) { return possibleActionNames[rndNum - 1]; }
 
     private void SetAgentPosition()
     {
         agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+    }
+
+    private void Animate()
+    {
+        normalizedMovement = agent.desiredVelocity.normalized;
+        forwardVector = Vector3.Project(normalizedMovement, transform.up);
+        rightVector = Vector3.Project(normalizedMovement, transform.right);
+
+        if (forwardVector.y > 0.10f) { animator.SetBool("Up", true); }
+        else { animator.SetBool("Up", false); }
+
+        if (forwardVector.y < -0.10f) { animator.SetBool("Down", true); }
+        else { animator.SetBool("Down", false); }
+
+        if (rightVector.x > 0) { animator.SetBool("Right", true); }
+        else { animator.SetBool("Right", false); }
+
+        if (rightVector.x < 0) { animator.SetBool("Left", true); }
+        else { animator.SetBool("Left", false); }
     }
 
 }
